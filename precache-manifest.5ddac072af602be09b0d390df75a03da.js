b5fc99bb73df0126ae003eb5e6e8ed92c820db0b
Wan-Ti <1826155752@qq.com>
2020-03-16 09:18:31 +0800

    deploy

---
+++ precache-manifest.5ddac072af602be09b0d390df75a03da.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8d990e23f6555d551c33",
+    "url": "/flower-money/css/app.9ed9faf7.css"
+  },
+  {
+    "revision": "9ce2e1ff9eaa11e27f0ff5f4a61ff0ff",
+    "url": "/flower-money/index.html"
+  },
+  {
+    "revision": "8d990e23f6555d551c33",
+    "url": "/flower-money/js/app.9e8c639e.js"
+  },
+  {
+    "revision": "7716c195ccd91595b01d",
+    "url": "/flower-money/js/chunk-vendors.4e887dfd.js"
+  },
+  {
+    "revision": "f3aaee380deddea3de78c3de33e81ce5",
+    "url": "/flower-money/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/flower-money/robots.txt"
+  }
+]);
